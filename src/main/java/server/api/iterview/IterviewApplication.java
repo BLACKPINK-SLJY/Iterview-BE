3b876f293600a7612480df2ce9dfa31e2e23a85e
Seungwoo, Joo <thisis.joos@gmail.com>
2023-05-13 10:47:00 +0900

    init: settings

---
+++ src/main/java/server/api/iterview/IterviewApplication.java
@@ -0,0 +1,13 @@
+package server.api.iterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IterviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IterviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/server/api/iterview/IterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package server.api.iterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
