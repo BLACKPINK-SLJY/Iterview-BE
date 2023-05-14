f2852ccaf4bdd6857e92b562836832d75b94894c
Seungwoo, Joo <thisis.joos@gmail.com>
2023-05-14 15:24:57 +0900

    feat: initial code format

---
+++ src/main/java/server/api/iterview/repository/FooRepository.java
@@ -0,0 +1,18 @@
+package server.api.iterview.repository;
+
+import org.springframework.stereotype.Repository;
+import server.api.iterview.domain.Foo;
+
+
+import java.util.Optional;
+
+@Repository
+public class FooRepository {
+
+    public Optional<Foo> findFoo(String name, String title){
+        Foo foo = new Foo(name, title);
+
+        return Optional.of(foo);
+    }
+
+}
