3c4002ad01ccf8ce87bcf9bf6c07dcf6d3842e84
Seungwoo, Joo <thisis.joos@gmail.com>
2023-05-23 11:26:18 +0900

    refactor: modify response fotmat(to array) - tag, keyword

--- src/main/java/server/api/iterview/dto/question/QuestionDto.java
+++ src/main/java/server/api/iterview/dto/question/QuestionDto.java
@@ -19,30 +19,29 @@ public class QuestionDto {
     private Long id;
     private String content;
     private String category;
-    private List<KeywordDto> keywords;
-    private List<TagDto> tags;
+    private List<String> keywords;
+    private List<String> tags;
     private Integer level;
 
     public static QuestionDto of(Question question){
 //        Optional<String> tagString =  question.getTags().stream().map(Tag::getName).reduce((x, y) -> x + ", " + y);
 
-        Object[] keywordStringList = Arrays.stream(question.getKeywords().split(",")).map(String::trim).toArray();
-        List<KeywordDto> keywordDtos = new ArrayList<>();
-        for(Object keywordString : keywordStringList){
-            keywordDtos.add(KeywordDto.of((String)keywordString));
+        List<String> keywordStrings = new ArrayList<>();
+        for(Object keyword: Arrays.stream(question.getKeywords().split(",")).map(String::trim).toArray()){
+            keywordStrings.add((String)keyword);
         }
 
-        List<TagDto> tagDtos = new ArrayList<>();
-        for (Tag tag : question.getTags()){
-            tagDtos.add(TagDto.of(tag));
+        List<String> tagStrings = new ArrayList<>();
+        for (Tag tag: question.getTags()){
+            tagStrings.add(tag.getName());
         }
 
         return QuestionDto.builder()
                 .id(question.getId())
                 .content(question.getContent())
                 .category(question.getCategory().name())
-                .keywords(keywordDtos)
-                .tags(tagDtos)
+                .keywords(keywordStrings)
+                .tags(tagStrings)
                 .level(question.getLevel())
                 .build();
     }
