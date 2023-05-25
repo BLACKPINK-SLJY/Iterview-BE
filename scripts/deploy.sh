d5c9ffc6e647f4ef46a95905c4ca3ed90b211e96
Seungwoo, Joo <thisis.joos@gmail.com>
2023-05-25 12:49:35 +0900

    fix: deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 echo "현재 구동중인 애플리케이션 pid: $CURRENT_PID"
 
-if [ -z "$CURRENT_PID" ]; then            #7
+if [ -z "$CURRENT_PID" ]; then
         echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
     echo "> kill -9 $CURRENT_PID"
