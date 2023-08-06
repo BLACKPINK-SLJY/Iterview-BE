d77fa0a3b8ea410ca9c16fb053b2ef9e6b92ee89
Seungwoo, Joo <thisis.joos@gmail.com>
2023-08-06 18:53:19 +0900

    fix: cors origin

--- src/main/java/server/api/iterview/config/CorsConfig.java
+++ src/main/java/server/api/iterview/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
+        config.addAllowedOrigin("https://iterview.com/");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
 
